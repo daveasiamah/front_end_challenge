d8454098e76c85ac6407d365ef154531811ee848
daveasiamah <daveasiamah@gmail.com>
2021-07-24 09:54:35 +0000

    initial commit and creating component containers

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import SearchLocation from '../Search/SearchLocation'
+
+function Header() {
+    return (
+        <div>
+            <SearchLocation/>
+           <h2>Header</h2>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/components/containers/Search/SearchLocation.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SearchLocation() {
+    return (
+        <div>
+            <h2>Search Location</h2>
+        </div>
+    )
+}
+
+export default SearchLocation
