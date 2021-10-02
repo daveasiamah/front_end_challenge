1ee4a104c2cfd6678a3612afd535b2da1cf29c88
daveasiamah7 <daveasiamah7@gmail.com>
2021-10-02 12:13:45 +0000

    added overview ready to continue

--- src/components/containers/Overview/Overview.js
+++ src/components/containers/Overview/Overview.js
@@ -44,7 +44,6 @@ const ContactActions = styled.div`
   display: flex;
   flex-direction: row;
   align-items: flex-start;
-  /* justify-content: space-around; */
   color: ${colors.textColor};
   font-size: 14px;
 `;
---
+++ src/tests/Header.test.js
@@ -0,0 +1,7 @@
+import { render } from "@testing-library/react";
+
+import Header from "../components/Header/Header";
+
+it("Renders a header", () => {
+  render(<Header />);
+});
